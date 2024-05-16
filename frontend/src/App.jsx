20dc22996d97b80f064b80d0106566e5ae72dc0c
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-05-16 11:54:49 -0400

    added topNavigationbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
     return (
       <div className="App">
-        <TopicList />
+        <TopNavigationBar />
         <PhotoList />
       </div>
     );
