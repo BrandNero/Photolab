d5c5959d9347a71ecdda1b2757bdd9f1a411babd
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-05-15 21:45:42 -0400

    PhotoListItem now properly loads

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,12 +17,7 @@ const sampleDataForPhotoListItem = {
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem 
-      key={sampleDataForPhotoListItem.id}
-      location={sampleDataForPhotoListItem.location}
-      imageSource={sampleDataForPhotoListItem.imageSource}
-      username={sampleDataForPhotoListItem.username}
-      profile={sampleDataForPhotoListItem.profile} />
+      <PhotoListItem photo={sampleDataForPhotoListItem} />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,21 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({photo}) => {
+  return (
+    <article className="photo-list__item">
+      <header>
+      <img src={photo.imageSource} alt={`${photo.username}'s photo`} className="photo-list__image" />
+      </header>
+      <section className="photo-list__user-details">
+        <img src={photo.profile} alt={`${photo.username}'s profile`} className="photo-list__user-profile" />
+        <section className="photo-list__user-info">
+          <p>{photo.username}</p>
+          <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        </section>
+      </section>
+    </article>
+  );
 };
 
 export default PhotoListItem;
