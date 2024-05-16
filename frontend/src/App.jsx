a5d85de16c0cf04eaa3683e54bf0e6302d339b99
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-05-16 11:32:22 -0400

    added topicList plus logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 import PhotoList from 'components/PhotoList';
 import './App.scss';
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
     return (
       <div className="App">
-        {/* {photos.map((photo, index) => (
-          <PhotoListItem key={index} photo={photo} /> */}
-        {/* ))} */}
+        <TopicList />
         <PhotoList />
       </div>
     );
