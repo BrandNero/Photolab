35becf739434b5f84ff5d30ab695020f33eca542
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-05-16 23:04:15 -0400

    uses homeRoute as parent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
 import './App.scss';
-import TopNavigationBar from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
     return (
       <div className="App">
-        <TopNavigationBar />
-        <PhotoList />
+        <HomeRoute />
       </div>
     );
 };
